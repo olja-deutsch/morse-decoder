0ce7577435784cf4bb9569784c9483445d6128c2
Olga Murina <olgamurina92@gmail.com>
2020-10-10 18:21:14 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const matrix       = [],
+          array        = expr.split(''),
+          morseMatrix  = [];
+    let resultString = '',
+        morseLetter  = '';
+
+    for (let i = 0; i < array.length; i += 10) {
+        if (array[i] !== '*') {
+            matrix.push(array.slice(i, i + 10));
+        } else {
+            matrix.push([' ']);
+        };
+    };
+
+    for (let i = 0; i < matrix.length; i += 1) {
+        const currentArray = matrix[i]; 
+
+        if (currentArray.length === 1) {
+            resultString += ' ';
+        } else {
+            for (let j = 0; j < currentArray.length; j += 2) {
+                if (currentArray[j] + currentArray[j + 1] === '10') {
+                    morseLetter += '.';
+                } else if (currentArray[j] + currentArray[j + 1] === '11') {
+                    morseLetter += '-';
+                };
+            };
+
+            resultString += MORSE_TABLE[morseLetter];
+
+            morseLetter = '';
+        };
+    };
+
+    return resultString;
 }
 
 module.exports = {
